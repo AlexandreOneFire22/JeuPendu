1a2872f249fdd31229a240be5921a0f4f8a5f0a7
GAUTHIER Alexandre <alexgau70@gmail.com>
2024-10-08 15:42:05 +0200

    TP pendu fin.

--- src/JeuPendu.php
+++ src/JeuPendu.php
@@ -8,11 +8,6 @@ class JeuPendu{
     private int $nbTentativeRestante;
     private array $lettrePropose;
 
-    /**
-     * @param MotADeviner $motADeviner
-     * @param int $nbTentativeRestante
-     * @param array $lettrePropose
-     */
     public function __construct(){
         $this->motADeviner = new MotADeviner();
         $this->nbTentativeRestante = 7;
@@ -24,17 +19,30 @@ class JeuPendu{
         echo "Bienvenue au jeu du Pendu ^^ \n";
         echo PHP_EOL;
 
+        //Tant que le mot à deviner n'est pas complet et qu'il reste des tentatives au joueur alors le jeu continue.
+        //Autrement dit, tant qu'il n'a pas gagné (trouvé le mot) ou perdu (épuisé toute ses tentatives) le joueur rejoue.
         while (!$this->motADeviner->isComplete() && $this->nbTentativeRestante>0){
-            echo "Mot à Deviner : ".$this->afficherMotCache();
+
+            echo "Mot à Deviner : ";
+            //affiche la progession du mot à compléter (exemple "t___te").
+            echo $this->afficherMotCache();
             echo PHP_EOL;
-            echo "Lettre essayée : [".implode(" ,",$this->lettrePropose)."].";
+
+            //Montre au joueur tous ses essayes non valide.
+            echo "Lettre essayée : [".implode(" ,",$this->lettrePropose)."]";
             echo PHP_EOL;
-            $this->traiterLettre($this->demanderLettre());
-            echo PHP_EOL;
+
+            //Affiche le nombre de tentative restante.
             echo "Nombre de tentative restante : ".$this->nbTentativeRestante;
             echo PHP_EOL;
 
+            //Traite la lettre donnée, réalise les modifications en conséquense si la lettre est bonne ou non.
+            $this->traiterLettre($this->demanderLettre());
+            echo PHP_EOL;
+
         }
+
+        //Une fois le jeu arrêté on verifie si il s'est arrêté à cause d'une victoire ou d'une défaite.
         if ($this->motADeviner->isComplete()){
             echo "Bravo vous avez gagné ^^, Le mot à deviner était ".$this->motADeviner->getMot().".";
         }else{
@@ -44,43 +52,33 @@ class JeuPendu{
     }
 
     public function afficherMotCache() : void {
+        //Affiche le mot caché (étant sous forme de tableau) sous forme de chaîne de caractères.
         echo implode("",$this->motADeviner->getMotCache());
     }
 
     public function demanderLettre() : string {
+        //Demande à l'utilisateur de rentrer une lettre.
         $lettre = readline("Saisissez une lettre : ");
-        return $lettre;
+        //Verifie si la lettre est bien une lettre et si elle n'est pas déjà présente dans le tableau des tentatives.
+        if (is_string($lettre)&&!in_array($lettre,$this->lettrePropose)){
+            return $lettre;
+        }
+        //Redemande à l'utilisateur de rentrer une lettre tant que celle-ci n'est pas valide.
+        return $this->demanderLettre();
     }
 
     public function traiterLettre(string $lettre) : void {
-
+        //Si la lettre donnée est bien dans le mot à trouvé
         if (str_contains($this->motADeviner->getMot(),$lettre)){
+            //Alors appel de la méthode "updateMotCache" qui réalise le ou les changements sur le mot cache.
             $this->motADeviner->updateMotCache($lettre);
         }else{
+            //Si la lettre n'est pas dans le mot alors le joueur perd une tentative.
             $this->nbTentativeRestante -= 1;
+            //La lettre est rajoutée dans le tableau des tentatives.
+            $this->lettrePropose [] = $lettre;
         }
 
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
--- src/MotADeviner.php
+++ src/MotADeviner.php
@@ -8,15 +8,16 @@ class MotADeviner{
     private string $motADeviner;
     private array $motCache;
 
-    /**
-     * @param array $listeMots
-     * @param string $motADeviner
-     * @param array $motCache
-     */
+
     public function __construct()
     {
-        $this->listeMots = ["test","logiciel","ordinateur"];
+        //Récupère sous forme de tableau la liste de mots présent dans le fichier "listeMots.txt".
+        $this->listeMots = explode("\n",file_get_contents(__DIR__."/listeMots.txt"));
+
+        //Prend aléatoirement un mot du tableau "$this->listeMots".
         $this->motADeviner = $this->listeMots[rand(0,count($this->listeMots)-1)] ;
+
+        //Crée un tableau composé de "_" de la même taille que le mots "$this->motADeviner".
         $this->motCache = str_split(str_repeat("_",strlen($this->motADeviner)));
     }
 
@@ -30,15 +31,22 @@ class MotADeviner{
 
     public function updateMotCache(string $lettre) : void {
 
+        //Crée un tableau qui contient lettre par lettre le mot à faire deviner.
         $motADeviner = str_split($this->motADeviner);
+
         foreach ($motADeviner as $index=>$caractere){
+
+            //Si la lettre rentrer par le joueur est égale à une lettre du mot.
             if ($caractere == $lettre){
+
+                //Alors à l'emplacement de la lettre trouvé, la lettre prendra la place du "_" dans le mots caché (composer que de "_").
                 $this->motCache[$index] = $lettre;
             }
         }
     }
 
     public function isComplete() : bool{
+        //Renvoie True si le mot à été entièrement trouvé, sinon renvoie False.
         return (implode("",$this->motCache)) == $this->motADeviner;
     }
 
